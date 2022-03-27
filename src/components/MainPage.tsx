baf605291bd9f1be2965a5bc7fc6098759a3133b
Natsume <2496347060@qq.com>
2022-03-27 23:49:01 +0800

    feat(): init scss react ts on windows

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MainPage.scss'
+
+export const MainPage=()=>{
+    return(
+        <div className='main-page'>
+            Fuck react FUCK windows
+        </div>
+    )
+}
