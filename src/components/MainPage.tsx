6d6cae106e6d3453712ccd9d4e457a5dea669a7e
Natsunokazeme <79150294+Natsunokazeme@users.noreply.github.com>
2022-08-04 00:02:41 +0800

    feat(): set tailwind css and add time log

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import React from "react";
 import "./MainPage.scss";
 
@@ -19,12 +20,26 @@ export const MainPage = (prop: any) => {
     };
   };
 
+  const logTimeToStorage = () => {
+    let rawTimeLogs = localStorage.getItem("timelog") ?? "[]";
+    let timeLogs = JSON.parse(rawTimeLogs);
+    if (!timeLogs || timeLogs.length === 0) {
+      timeLogs = [new Date().toString()];
+    } else {
+      timeLogs.push(new Date().toString());
+    }
+    localStorage.setItem("timelog", JSON.stringify(timeLogs));
+  };
+
   const time = new Date();
   return (
     <div className="main-page text-red-500">
       <section>
         <h5>current time:{time.toISOString()}</h5>
-        <button onClick={startWebSocket}></button>
+        <Button variant="contained" onClick={logTimeToStorage}>
+          Record Time
+        </Button>
+        <button onClick={startWebSocket}> websocket</button>
       </section>
     </div>
   );
